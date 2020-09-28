db39849908cda0bb9b26f66d518ab85a1cb0317a
Official21A <70829014+Official21A@users.noreply.github.com>
2020-09-28 08:10:13 +0330

    Final edit

--- script.py
+++ script.py
@@ -1,41 +1,63 @@
 # continue
 
 import requests
+from plotly.graph_objs import Bar
+from plotly import offline
 
 
-url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars' # main url
 headers = {'Accept': 'application/vnd.github.v3+json'}
 
 r = requests.get(url, headers=headers) # this is how we send our requests
 
-print(f'Status code: {r.status_code}')
+print(f'Status code: {r.status_code}') # getting the response status code
 
 response_dict = r.json() # it reads all the json data
-
-print(f'Total repositories: {response_dict["total_count"]}') # total amount of the repos with python
-
 repo_dicts = response_dict['items'] # a dictionary of repositories
-print(f'Repositories returned: {len(repo_dicts)}')
+repo_links, stars, labels = [], [], []
 
-repo_dict_0 = repo_dicts[0] # getting the first item
-print(f'\nKeys: {len(repo_dict_0)}')
-for key in sorted(repo_dict_0.keys()):
-	print(key)
-
-print('\nSelected information about each repository:') # then we print some of the infomration of the results
 for repo_dict in repo_dicts:
-	print(f'Name: {repo_dict["name"]}')
-	print(f'Owner: {repo_dict["owner"]["login"]}')
-	print(f'Stars: {repo_dict["stargazers_count"]}')
-	print(f'Repository: {repo_dict["html_url"]}')	
-	print(f'Created: {repo_dict["created_at"]}')
-	print(f'Updated: {repo_dict["updated_at"]}')
-	print(f'Description: {repo_dict["description"]}')
+	# creating a link type html for the xaxises
+	repo_name = repo_dict['name']
+	repo_url = repo_dict['html_url']
+	repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+	repo_links.append(repo_link)
+	# starts as yaxises
+	stars.append(repo_dict['stargazers_count'])
+	# owere infor for hover text
+	owner = repo_dict['owner']['login']
+	description = repo_dict['description']
+	label = f'{owner}<br />{description}'
+	labels.append(label)
 
 
-'''
-	So each api has a limit for requests per minute.
-	In this case we can see out status in limitaion via 
-	https://api.github.com/rate_limit.
-	Each url api has its own limits.
-'''	
+# just like working with data we use bar to show the results of responce
+data = [{
+	'type': 'bar',
+	'x': repo_links,
+	'y': stars,
+	'hovertext': labels,
+	'marker': {
+		'color': 'rgb(60, 100, 150)',
+		'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'},
+	},
+	'opacity': 0.6,
+}]	
+
+my_layout = {
+	'title': 'Most-Starred Python Projects on Github',
+	'titlefont': {'size':28},
+	'xaxis': {
+		'title': 'Repository',
+		'titlefont': {'size': 24},
+		'tickfont': {'size': 14},
+	},
+	'yaxis': {
+		'title': 'Stars',
+		'titlefont': {'size': 24},
+		'tickfont': {'size': 14},
+	},
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='python_repos.html') # saving file into *.html
